cf5611e55f4297c7f8e0a71bbf1a1ce838215f14
Timo Runge <me@timorunge.com>
2018-09-17 17:04:18 +0200

    * Better Ansible linting

--- tests/docker/docker-entrypoint.sh
+++ tests/docker/docker-entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 set -e
 
-printf "[defaults]\nroles_path=/etc/ansible/roles\n" > ansible.cfg
+printf "[defaults]\nroles_path=/etc/ansible/roles\n" > /ansible/ansible.cfg
 
-ansible-lint /ansible/test.yml
-ansible-lint /etc/ansible/roles/${ansible_role}/tasks/main.yml
+if [ ! -f /etc/ansible/lint.zip ]; then
+  wget https://github.com/ansible/galaxy-lint-rules/archive/master.zip -O \
+  /etc/ansible/lint.zip
+  unzip /etc/ansible/lint.zip -d /etc/ansible/lint
+fi
+
+ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint -r \
+  /etc/ansible/lint/galaxy-lint-rules-master/rules \
+  /etc/ansible/roles/${ansible_role}
+ansible-lint -c /etc/ansible/roles/${ansible_role}/.ansible-lint -r \
+  /etc/ansible/lint/galaxy-lint-rules-master/rules \
+  /ansible/test.yml
 
 ansible-playbook /ansible/test.yml \
   -i /ansible/inventory \
